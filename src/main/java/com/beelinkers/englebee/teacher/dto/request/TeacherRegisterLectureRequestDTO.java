4b6ee12099cba0880278559d32b042cfb2b91e3f
yeong1203 <cozyluv117@gmail.com>
2024-09-08 16:57:36 +0900

    chore: 수업 생성 관련 DTO 수정

--- src/main/java/com/beelinkers/englebee/teacher/dto/request/TeacherRegisterLectureRequestDTO.java
+++ src/main/java/com/beelinkers/englebee/teacher/dto/request/TeacherRegisterLectureRequestDTO.java
@@ -4,14 +4,16 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class TeacherRegisterLectureRequestDTO {
 
   private String studentNickname;
-  private String letureTitle;
+  private String lectureTitle;
   private List<TeacherRegisterLectureSubjectLevelDTO> subjectLevels;
 
 }
